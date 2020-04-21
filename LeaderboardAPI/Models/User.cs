63c20550764c00cf342bce03c4c77343b395fab2
Hugo <hugoruivo@gmail.com>
2020-04-21 02:37:05 +0100

    Added user controller. Added Required field to username model.

--- LeaderboardAPI/Models/User.cs
+++ LeaderboardAPI/Models/User.cs
@@ -9,6 +9,7 @@ namespace LeaderboardAPI.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
+        [BsonRequired]
         public string UserName { get; set; }
     }
 }
